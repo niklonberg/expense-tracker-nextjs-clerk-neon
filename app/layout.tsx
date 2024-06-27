0c4f23ecae00b11ab223fc2f570bd0d77f0f0f0a
Nikolai <niklonberg@gmail.com>
2024-06-27 11:16:46 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Expense tracker",
+  description: "Track expenses & create budget",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
