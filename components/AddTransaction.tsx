3f7f95acf85c910b04bc593bc0aed8b875a83c21
Nikolai <niklonberg@gmail.com>
2024-06-28 11:14:59 +0200

    add AddTransaction comp.

---
+++ components/AddTransaction.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+
+const AddTransaction = () => {
+  async function clientAction(formData: FormData) {
+    console.log(formData.get("text"), formData.get("amount"));
+  }
+
+  return (
+    <>
+      <h2>Add transaction</h2>
+      <form action={clientAction}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" name="text" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br /> (negative → expense, positive → income)
+          </label>
+          <input type="number" name="amount" step={0.01} />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransaction;
