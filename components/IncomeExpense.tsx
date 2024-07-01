724819fec52b56eb9a4426a35f65b1404a6362b8
Nikolai <niklonberg@gmail.com>
2024-07-01 11:37:25 +0200

    add IncomeExpense comp.

---
+++ components/IncomeExpense.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const IncomeExpense = () => {
+  return (
+    <div className="income-expense-container">
+      <section>
+        <h4>Income</h4>
+        <p className="money positive">$600</p>
+      </section>
+      <section>
+        <h4>Expense</h4>
+        <p className="money negative">$200</p>
+      </section>
+    </div>
+  );
+};
+
+export default IncomeExpense;
