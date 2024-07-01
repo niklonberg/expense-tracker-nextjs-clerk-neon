23cbaf7b352bedc24fd7610a858ba227861252ce
Nikolai <niklonberg@gmail.com>
2024-07-01 11:15:23 +0200

    add form reset on submit

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -4,6 +4,7 @@ import addTransaction from "@/app/actions/addTransaction";
 import { toast } from "react-toastify";
 
 const AddTransaction = () => {
+  const formRef = React.useRef<HTMLFormElement>(null);
   async function clientAction(formData: FormData) {
     const { data, error } = await addTransaction(formData);
 
@@ -13,13 +14,14 @@ const AddTransaction = () => {
       // temp, redirect later
       toast.success("Transaction added");
       console.log(data);
+      formRef.current?.reset();
     }
   }
 
   return (
     <>
       <h2>Add transaction</h2>
-      <form action={clientAction}>
+      <form ref={formRef} action={clientAction}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" name="text" />
