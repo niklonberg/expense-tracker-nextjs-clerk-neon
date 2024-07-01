2daaa3c470e70d1ff5b1a49ab7cc3801ec47a734
Nikolai <niklonberg@gmail.com>
2024-07-01 13:14:42 +0200

    add button, handleDeleteTransaction handlerfn

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -6,12 +6,30 @@ import { toast } from "react-toastify";
 const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
   const sign = transaction.amount > 0 ? "+" : "-";
 
+  async function handleDeleteTransaction(transactionId: string) {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this transaction?"
+    );
+
+    if (!confirm) return;
+
+    await deleteTransaction(transactionId);
+
+    toast.success("Transaction deleted");
+  }
+
   return (
     <li className={transaction.amount > 0 ? "positive" : "negative"}>
       {transaction.text}
       <span>
         {sign}${addThousandNumberCommas(Math.abs(transaction.amount))}
       </span>
+      <button
+        className="delete-btn"
+        onClick={() => handleDeleteTransaction(transaction.id)}
+      >
+        x
+      </button>
     </li>
   );
 };
