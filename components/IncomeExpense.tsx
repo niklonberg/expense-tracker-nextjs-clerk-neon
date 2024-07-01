914794918750dd0f361f39250487f9d8ab25d63f
Nikolai <niklonberg@gmail.com>
2024-07-01 12:04:39 +0200

    use getIncomeExpense, ensure overly long decimal points are removed

--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -1,15 +1,27 @@
 import React from "react";
+import getIncomeExpense from "@/app/actions/getIncomeExpense";
+import { addThousandNumberCommas } from "@/lib/utils";
 
-const IncomeExpense = () => {
+const IncomeExpense = async () => {
+  const { income, expenses } = await getIncomeExpense();
+
+  // Ensure income and expenses are always numbers
+  const formattedIncome = parseFloat(income?.toFixed(2) ?? "0");
+  const formattedExpenses = parseFloat(expenses?.toFixed(2) ?? "0");
+
   return (
     <div className="income-expense-container">
       <section>
         <h4>Income</h4>
-        <p className="money positive">$600</p>
+        <p className="money positive">
+          ${addThousandNumberCommas(formattedIncome)}
+        </p>
       </section>
       <section>
         <h4>Expenses</h4>
-        <p className="money negative">$200</p>
+        <p className="money negative">
+          ${addThousandNumberCommas(formattedExpenses)}
+        </p>
       </section>
     </div>
   );
