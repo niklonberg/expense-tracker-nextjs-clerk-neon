5c93944afcd2b679255192d00a8d765d96659420
Nikolai <niklonberg@gmail.com>
2024-07-01 13:09:04 +0200

    add sign to indicate +-

--- components/TransactionItem.tsx
+++ components/TransactionItem.tsx
@@ -4,10 +4,14 @@ import { addThousandNumberCommas } from "@/lib/utils";
 import { toast } from "react-toastify";
 
 const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
+  const sign = transaction.amount > 0 ? "+" : "-";
+
   return (
     <li className={transaction.amount > 0 ? "positive" : "negative"}>
       {transaction.text}
-      <span>{addThousandNumberCommas(transaction.amount)}</span>
+      <span>
+        {sign}${addThousandNumberCommas(Math.abs(transaction.amount))}
+      </span>
     </li>
   );
 };
